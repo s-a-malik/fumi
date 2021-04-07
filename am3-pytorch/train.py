dc9da52dae0a31760c67b8b6c4496acdc4299443
s-a-malik <shreshth_malik@yahoo.co.uk>
2021-04-07 16:27:49 +0100

    am3 initial structure

---
+++ am3-pytorch/train.py
@@ -0,0 +1,19 @@
+"""Training loop for AM3.
+"""
+
+
+def main(args):
+    pass
+
+
+def evaluate(model, test_loader):
+    """Evaluate model on test set
+    """
+
+
+if __name__ == "__main__":
+
+    # read cmd line arguments
+    args = parse_args()
+
+    main(args)
