392b753eef1763dac49fb09a6ab2c0bbbb77714b
Yousuf Mohamed-Ahmed <youmed.tech@gmail.com>
2021-04-21 09:16:56 +0100

    lessen RAM burden by operating on single vectors

--- dataset/data.py
+++ dataset/data.py
@@ -164,7 +164,7 @@ class SupervisedZanim(torch.utils.data.Dataset):
 		# pooling(self.model(
 			# input_ids=self._zcd.descriptions, attention_mask=self._zcd.mask).last_hidden_state)
 		for i, desc in enumerate(self._zcd.descriptions):
-			self._bert_embeddings[i] = self.model(input_ids=desc.unsqueeze(), attention_mask=self._zcd.mask[i].unsqueeze()).last_hidden_state
+			self._bert_embeddings[i] = self.model(input_ids=desc.unsqueeze(0), attention_mask=self._zcd.mask[i].unsqueeze(0)).last_hidden_state
 
 		self._bert_embeddings = pooling(self._bert_embeddings)
 
