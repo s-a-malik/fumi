764edcdfb002e54372bd1a8782bdd436d0f47106
Yousuf Mohamed-Ahmed <youmed.tech@gmail.com>
2021-05-26 12:33:40 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="zeroshot",
+    name="zanim",
     version="0.0.1",
     author=
     " Matthew Jackson, Shreshth Malik, Michael Matthews, Yousuf Mohamed-Ahmed",
@@ -14,7 +14,20 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/s-a-malik/zero-shot",
-    install_requires=['torch>=1.0.0'],
+    install_requires=[
+        'torch>=1.0.0',
+        "tqdm==4.60.0",
+        "torchvision==0.9.1",
+        "torch==1.8.1",
+        "numpy==1.20.2",
+        "torchmeta==1.7.0",
+        "h5py==3.2.1",
+        "wandb==0.10.26",
+        "pandas==1.2.4",
+        "gensim==4.0.1",
+        "nltk==3.6.1",
+        "transformers==4.5.1",
+    ],
     include_package_data=True,
     packages=["zanim"],
     python_requires=">=3.6",
